6ba68fd7166d28f0b398cfc1f11600a43309c0aa
Hoang Thu <hoangdt0308@gmail.com>
2020-09-29 17:13:36 +0700

    bbb

--- src/Server/Electric.Application/Handlers/ElectricMetter/UpdateElectricMetterHandler.cs
+++ src/Server/Electric.Application/Handlers/ElectricMetter/UpdateElectricMetterHandler.cs
@@ -25,7 +25,7 @@ namespace Electric.Application.Handlers
             _logger.LogInfo($"UpdateDeviceHandler(Id:{request.Id}, SeriaNumber:{request.SeriaNumber})");
 
             var electricEntity = DeviceMapper.Mapper.Map<ElectricMetter>(request);
-
+            int a = 1;
             if (electricEntity == null)
                 throw new ApplicationException($"Entity could not be mapped.");
 
